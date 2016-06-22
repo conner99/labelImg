afd4b2cbffc0523f6f5d3d5d9e2d878a2a19631a
conner99 <robin.ueffing@studmail.w-hs.de>
2016-06-22 14:06:10 +0200

    add labelImg.bat
    to start labelImg in Windows

---
+++ labelImg.bat
@@ -0,0 +1,9 @@
+@Echo off
+Echo start labelImg Batch
+:: https://github.com/conner99/labelImg
+:: forked from https://github.com/tzutalin/labelImg
+
+::set root_dir=C:%HOMEPATH%\labelImg-master
+::cd %root_dir%
+
+python labelImg.py
